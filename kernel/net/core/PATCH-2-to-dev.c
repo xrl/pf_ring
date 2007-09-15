304e90801616f0b9bc5928440c01fa1e52c4a71b
Luca Deri <deri@ntop.org>
2007-09-15 15:43:53 +0000

    Restored patch
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@3294 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- kernel/net/core/PATCH-2-to-dev.c
+++ kernel/net/core/PATCH-2-to-dev.c
@@ -1,14 +1,12 @@
 #if defined (CONFIG_RING) || defined(CONFIG_RING_MODULE)
 /*
-  This patch doesn't seem to be used anymore as it can result
-  in capturing the same packet twice
+  Incoming packets might be duplicated on non-NAPI drivers
+
   Marketakis Yannis <marketak@ics.forth.gr>
 */
-#if 0
 	if(ring_handler && ring_handler(skb, 1, 1)) {
 	  /* The packet has been copied into a ring */
 	  return(NET_RX_SUCCESS);
 	}
-#endif
 #endif /* CONFIG_RING */
 
